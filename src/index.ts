7722610d4eb3f7bcea0c52219dfc80e38f62eb47
= <eechemane29@gmail.com>
2022-07-16 13:33:16 +0800

    added dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Environment from 'constants/environment';
 import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = Environment.port();
