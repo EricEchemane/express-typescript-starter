2de0ab2b870b1c0dbe1a32d626ee73d70d7cf147
= <eechemane29@gmail.com>
2022-07-16 16:00:21 +0800

    test for heroku

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import type { Response } from "express";
 import express from 'express';
 import dotenv from "dotenv";
-import prisma from "prisma/client";
+// import prisma from "prisma/client";
 import environment from "constants/environment";
 dotenv.config();
 
 const app = express();
 
 app.get('/', async (_, res: Response) => {
-    const users = await prisma.user.findMany();
-    res.json({ users: users });
+    // const users = await prisma.user.findMany();
+    res.json({ users: "hello" });
 });
 
 app.listen(environment.port, () => {
