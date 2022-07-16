583fa635fc4587f9be79d36c8c59dc8fe64d2b5e
= <eechemane29@gmail.com>
2022-07-16 15:09:33 +0800

    prisma configured for this project

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+import type { Response } from "express";
 import express from 'express';
 import dotenv from "dotenv";
 import environment from 'constants/environment';
+import prisma from 'prisma/client';
 dotenv.config();
 
 const app = express();
 
-app.get('/', (_, res) => {
-    res.json({ message: 'hello' });
+app.get('/', async (_, res: Response) => {
+    const users = await prisma.user.findMany();
+    res.json({ users: users });
 });
 
 app.listen(environment.port, () => {
